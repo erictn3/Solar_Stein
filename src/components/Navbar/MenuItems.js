0c0fad52f527bb1e4c9968660f648cb944134e41
Eric Nguyen <erictn3@uci.edu>
2021-05-05 12:41:47 -0700

    first commit

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,23 @@
+export const MenuItem = [
+  {
+    title: "Home",
+    url: "#",
+    cName: "nav-links"
+  },
+  {
+    title: "Solar Index",
+    url: "#",
+    cName: "nav-links"
+  },
+  {
+    title: "Profile",
+    url: "#",
+    cName: "nav-links"
+  },
+  {
+    title: "Sign Up",
+    url: "#",
+    cName: "nav-links-mobile"
+  },
+]
+
