e8d9b248ec5e70b14515c246be7dde66c73aeb6d
Eric Nguyen <erictn3@uci.edu>
2021-05-11 20:16:34 -0700

    created handletagclick functions

--- client/src/components/Jobs/Job.js
+++ client/src/components/Jobs/Job.js
@@ -1,63 +1,75 @@
-import React from 'react';
+import React from "react";
 
-const Job = ({ job: {
-  company,
-  logo,
-  isNew,
-  featured,
-  position,
-  role,
-  level,
-  postedAt,
-  contract,
-  location,
-  languages,
-  tools, } }) => {
+const Job = ({
+  job: {
+    company,
+    logo,
+    isNew,
+    featured,
+    position,
+    role,
+    level,
+    postedAt,
+    contract,
+    location,
+    languages,
+    tools,
+  }, 
+  handleTagClick,
+}) => {
+  const jobTags = [role, level];
 
-    const jobTags = [role, level];
+  if (tools) {
+    jobTags.push(...tools);
+  }
 
-    if (tools) {
-      jobTags.push(...tools);
-    } 
-    
-    if (languages) {
-      jobTags.push(...languages)
-    }
+  if (languages) {
+    jobTags.push(...languages);
+  }
 
-  return(
-    <div className="flex bg-white shadow-lg m-4 p-6">
+  return (
+    <div
+      className={`flex flex-col bg-white shadow-lg my-16 mx-10 p-6 rounded ${
+        featured && "border-l-4 border-blue-500 border-solid"
+      } lg:flex-row lg:my-4` }>
       <div>
-        <img src={logo} alt={company}/>
+        <img className="-mt-16 mb-4 w-20 h-20 lg:h-24 lg:w-24 lg:my-0" src={logo} alt={company} />
       </div>
       <div className="flex flex-col justify-between ml-4">
-
         <h3 className="font-bold text-blue-400">
           {company}
           {isNew && (
-            <span className="text-blue-100 bg-blue-300 font-bold p-1 rounded-full m-2 py-1 px-2">
+            <span className="text-blue-100 bg-blue-300 font-bold p-1 rounded-full m-2 py-1 px-2 uppercase text-sm">
               New!
-              </span>)}
+            </span>
+          )}
           {featured && (
-            <span className="text-blue-100 bg-black font-bold p-1 rounded-full py-1 px-2">
+            <span className="text-blue-100 bg-black font-bold p-1 rounded-full py-1 px-2 uppercase text-sm">
               Featured
-              </span>)}
-
+            </span>
+          )}
         </h3>
 
-        <h2 className="font-bold text-xl">{position}</h2>
+        <h2 className="font-bold text-xl my-2 lg:my-0" >{position}</h2>
 
         <p className="text-gray-700">
           {postedAt} · {contract} · {location}
         </p>
-
       </div>
-      <div className="flex items-center justify-content ml-auto">
-        
-        { jobTags ? jobTags.map((jobTags) => <span className="text-blue-400 bg-blue-100 font-bold m-2 p-2">{jobTags}</span>) : ''}
-
+      <div className="flex flex-wrap items-center mt-4 mx-4 pt-4 border-t border-gray-500 border-solid lg:ml-auto lg:border-0 lg:pt-0 lg:mt-0 cursor-pointer">
+        {jobTags
+          ? jobTags.map((jobTags) => (
+              <span 
+              onClick={() => 
+                handleTagClick(jobTags)} 
+                className="text-blue-400 bg-blue-100 font-bold mr-4 mb-4 p-2 rounded lg:mb-0">
+                {jobTags}
+              </span>
+            ))
+          : ""}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Job;
--- client/src/components/Jobs/JobBoard.js
+++ client/src/components/Jobs/JobBoard.js
@@ -1,27 +1,73 @@
-import React, { useState, useEffect } from 'react';
-import Job from './Job';
-import data from '../../data.json';
- 
+import React, { useState, useEffect } from "react";
+import Job from "./Job";
+import data from "../../data.json";
 
 const Jobs = () => {
   const [jobs, setJobs] = useState([]);
+  const [filters, setFilters] = useState([]);
 
   useEffect(() => setJobs(data), []);
 
+  const filterFnc = ({ role, level, tools, languages }) => {
+    if (filters.length === 0) {
+      return true;
+    }
+
+    const jobTags = [role, level];
+
+    if (tools) {
+      jobTags.push(...tools);
+    }
+
+    if (languages) {
+      jobTags.push(...languages);
+    }
+
+    return filters.every(filter => jobTags.includes(filter));
+  };
+
+  const handleTagClick = (tag) => {
+    if (filters.includes(tag)) return;
+    setFilters([...filters, tag]);
+  };
+
+  const handleFilterClick = (passedFilter) => {
+    setFilters(filters.filter((f) => f !== passedFilter));
+  };
+
+  const clearFilters = () => {
+    setFilters([]);
+  }
+
+  const filteredJobs = jobs.filter(filterFnc);
+
   return (
-    <div>
-      {/* <h1 className='text-4xl'>Hello</h1> */}
-      {
-        jobs.length === 0 ? (
+    <>
+      <div className='container m-auto'>
+        {filters.length > 0 && (
+          <div
+            className={`flex bg-white shadow-lg -my-2 mb-16 mx-10 p-6 rounded relative`}>
+            {filters.map((filter) => (
+              <span
+                className="mr-4 mb-4 p-2 rounded font-bold cursor-pointer text-blue-500 bg-blue-100 lg:mb-0"
+                onClick={() => handleFilterClick(filter)}>
+                  x {filter}
+              </span>
+            ))}
+            <button onClick={clearFilters}
+            className="font-bold text-gray-700 ml-auto">Clear</button>
+          </div>
+        )}
+        {jobs.length === 0 ? (
           <p>Jobs are fetching...</p>
         ) : (
-          jobs.map((job) => <Job job={job} key=
-          {job.id} />)
-      )}     
-    </div>
-    
-    
-  )
-}
+          filteredJobs.map((job) => (
+            <Job job={job} key={job.id} handleTagClick={handleTagClick} />
+          ))
+        )}
+      </div>
+    </>
+  );
+};
 
 export default Jobs;
